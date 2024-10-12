b37535c467594c9c5e3bc82d054c765eb1f796ad
Vaibhav Mor <thevaimor@icloud.com>
2024-10-12 16:56:07 +1300

    refactor(vpc-setup): implement manual VPC configuration with CloudFormation constructs
    This commit refactors the VPC setup to use low-level CloudFormation constructs instead of the high-level CDK VPC construct. Key changes include:
    
    - Replace Vpc construct with manual CfnVPC configuration
    - Add Internet Gateway and VPC Gateway Attachment
    - Create public and private subnets in two availability zones
    - Implement public and private route tables
    - Configure routes and subnet associations
    
    The new setup provides:
    - More granular control over VPC resources
    - Explicit creation of public and private subnets
    - Manual configuration of route tables and Internet Gateway
    - Improved readability of the infrastructure setup
    
    This refactoring allows for better customisation of the VPC architecture and provides a clearer view of the underlying AWS resources being created.

--- src/VPC-and-Subnets/MyVpcStack.cs
+++ src/VPC-and-Subnets/MyVpcStack.cs
@@ -1,5 +1,6 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.EC2;
+using System.Collections.Generic;
 
 namespace VPC_and_Subnets;
 
@@ -7,24 +8,113 @@ public sealed class MyVpcStack : Stack
 {
     public MyVpcStack(Constructs.Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
-        // Create a new VPC
-        var vpc = new Vpc(this, "DemoVPC", new VpcProps
+        // Create a VPC manually
+        var cfnVpc = new CfnVPC(this, "DemoVPC", new CfnVPCProps
         {
-            VpcName = "DemoVPC",
-            AvailabilityZones = ["us-east-1a", "us-east-1b"],
-            IpAddresses = IpAddresses.Cidr("10.0.0.0/16"),
-            NatGateways = 0,
-            SubnetConfiguration = new SubnetConfiguration[]
-            {
-                new SubnetConfiguration
-                {
-                    CidrMask = 24,
-                    Name = "Public",
-                    SubnetType = SubnetType.PUBLIC,
-                }
-            },
-            EnableDnsHostnames = false,
+            CidrBlock = "10.0.0.0/16",
+            EnableDnsHostnames = true,
             EnableDnsSupport = true,
+            InstanceTenancy = "default",
+            Tags = new[] { new CfnTag { Key = "Name", Value = "DemoVPC" } }
+        });
+
+        // Create an Internet Gateway
+        var cfnInternetGateway = new CfnInternetGateway(this, "DemoIGW", new CfnInternetGatewayProps
+        {
+            Tags = new[] { new CfnTag { Key = "Name", Value = "DemoIGW" } }
+        });
+
+        // Attach the Internet Gateway to the VPC
+        new CfnVPCGatewayAttachment(this, "IGWAttachment", new CfnVPCGatewayAttachmentProps
+        {
+            VpcId = cfnVpc.Ref,
+            InternetGatewayId = cfnInternetGateway.Ref
+        });
+
+        // Create public subnets
+        var publicSubnetA = new CfnSubnet(this, "PublicSubnetA", new CfnSubnetProps
+        {
+            VpcId = cfnVpc.Ref,
+            AvailabilityZone = $"{this.Region}a",
+            CidrBlock = "10.0.1.0/24",
+            MapPublicIpOnLaunch = true,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Public Subnet A" } }
+        });
+
+        var publicSubnetB = new CfnSubnet(this, "PublicSubnetB", new CfnSubnetProps
+        {
+            VpcId = cfnVpc.Ref,
+            AvailabilityZone = $"{this.Region}b",
+            CidrBlock = "10.0.2.0/24",
+            MapPublicIpOnLaunch = true,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Public Subnet B" } }
+        });
+
+        // Create private subnets
+        var privateSubnetA = new CfnSubnet(this, "PrivateSubnetA", new CfnSubnetProps
+        {
+            VpcId = cfnVpc.Ref,
+            AvailabilityZone = $"{this.Region}a",
+            CidrBlock = "10.0.3.0/24",
+            MapPublicIpOnLaunch = false,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Private Subnet A" } }
+        });
+
+        var privateSubnetB = new CfnSubnet(this, "PrivateSubnetB", new CfnSubnetProps
+        {
+            VpcId = cfnVpc.Ref,
+            AvailabilityZone = $"{this.Region}b",
+            CidrBlock = "10.0.4.0/24",
+            MapPublicIpOnLaunch = false,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Private Subnet B" } }
+        });
+
+        // Create public route table
+        var publicRouteTable = new CfnRouteTable(this, "PublicRouteTable", new CfnRouteTableProps
+        {
+            VpcId = cfnVpc.Ref,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Public Route Table" } }
+        });
+
+        // Create private route table
+        var privateRouteTable = new CfnRouteTable(this, "PrivateRouteTable", new CfnRouteTableProps
+        {
+            VpcId = cfnVpc.Ref,
+            Tags = new[] { new CfnTag { Key = "Name", Value = "Private Route Table" } }
+        });
+
+        // Create public route
+        new CfnRoute(this, "PublicRoute", new CfnRouteProps
+        {
+            RouteTableId = publicRouteTable.Ref,
+            DestinationCidrBlock = "0.0.0.0/0",
+            GatewayId = cfnInternetGateway.Ref
+        });
+
+        // Associate public subnets with public route table
+        new CfnSubnetRouteTableAssociation(this, "PublicSubnetARouteTableAssociation", new CfnSubnetRouteTableAssociationProps
+        {
+            SubnetId = publicSubnetA.Ref,
+            RouteTableId = publicRouteTable.Ref
+        });
+
+        new CfnSubnetRouteTableAssociation(this, "PublicSubnetBRouteTableAssociation", new CfnSubnetRouteTableAssociationProps
+        {
+            SubnetId = publicSubnetB.Ref,
+            RouteTableId = publicRouteTable.Ref
+        });
+
+        // Associate private subnets with private route table
+        new CfnSubnetRouteTableAssociation(this, "PrivateSubnetARouteTableAssociation", new CfnSubnetRouteTableAssociationProps
+        {
+            SubnetId = privateSubnetA.Ref,
+            RouteTableId = privateRouteTable.Ref
+        });
+
+        new CfnSubnetRouteTableAssociation(this, "PrivateSubnetBRouteTableAssociation", new CfnSubnetRouteTableAssociationProps
+        {
+            SubnetId = privateSubnetB.Ref,
+            RouteTableId = privateRouteTable.Ref
         });
     }
 }
